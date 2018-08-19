96b07f211d4d9cc8ccbac99228a1313b6cd3963b
michaelKurowski <Byamarro@gmail.com>
2018-08-19 19:52:29 +0200

    Added threat class

--- core/threatsService/threatsService.class.ts
+++ core/threatsService/threatsService.class.ts
@@ -1,12 +1,12 @@
 import QueryOptions from './queryOptions.interface'
-
+import { Observable } from 'rxjs'
+import Threat from './threat.class'
 class ThreatsService {
     constructor() {
 
     }
 
-    getThreats(options: QueryOptions) {
-
+    getThreats(options: QueryOptions): Observable<Threat[]> {
     }
 }
 export default ThreatsService
