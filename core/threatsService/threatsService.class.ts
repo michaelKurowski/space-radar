8899bfcf642681f273470d050d644ac5cbd70988
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2018-08-19 21:44:51 +0200

    getThreats return mock of list of threats

--- core/threatsService/threatsService.class.ts
+++ core/threatsService/threatsService.class.ts
@@ -1,6 +1,18 @@
 import QueryOptions from '../../apiConnection/queryOptions.interface'
-
+import Threat from './threat.class'
+import {Observable, Observer} from 'rxjs'
 class ThreatsService {
+
+    private threatsMocks = [
+        new Threat(30,new Date(), new Date(), 3),
+        new Threat(12,new Date(), new Date(), 1),
+        new Threat(5,new Date(), new Date(), 0),
+        new Threat(54,new Date(), new Date(), 1),
+        new Threat(10,new Date(), new Date(), 3),
+        new Threat(9,new Date(), new Date(), 1),
+        new Threat(2,new Date(), new Date(), 0),
+    ]
+
     constructor() {
 
     }
@@ -9,8 +21,10 @@ class ThreatsService {
 
     }
 
-    getThreats(days:number, body:string, dangerLevel: number) {
-        
+    getThreats(days:number, body:string, dangerLevel: number): Observable<Threat[]> {
+        return Observable.create((observer: Observer<Threat[]>) => {
+            observer.next(this.threatsMocks)
+        } )
     }
 }
 export default ThreatsService
