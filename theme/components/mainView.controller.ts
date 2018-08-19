3e75c983b6db7d9db5920aea5159519546243804
michaelKurowski <Byamarro@gmail.com>
2018-08-19 22:57:47 +0200

    Table of records added

--- theme/components/mainView.controller.ts
+++ theme/components/mainView.controller.ts
@@ -1,11 +1,31 @@
 import ThreatsService from '../../core/threatsService/threatsService.class'
+import Threat from '../../core/threatsService/threat.class'
 class MainView {
     public x: number
     private threatsService: ThreatsService
-    constructor(threatsService: ThreatsService) {
+    private $timeout: ng.ITimeoutService
+    public threats: Threat[]
+    public viewer: number[]
+    constructor(threatsService: ThreatsService, $timeout: ng.ITimeoutService) {
         this.x = 2
+        this.$timeout = $timeout
         this.threatsService = threatsService
+        this.threats = []
+        this.viewer = []
+        this.updateThreats()
+    }
+
+    updateThreats() {
+        console.log('updateThreats')
+        this.threatsService.getThreats(0, 'Earth', 2)
+            .subscribe((threats: Threat[]) => {
+                this.$timeout(() => {
+                    this.threats = threats
+                    this.viewer = threats.map((thr: Threat) => thr.getSize())
+                    console.log(threats, this.viewer)
+                })
+            })
     }
 }
 
-export default ['threatsService', MainView]
+export default ['threats', '$timeout', MainView]
