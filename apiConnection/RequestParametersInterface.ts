309bd82c74d1931044b7d04d3e6c4ad6205b3f46
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2018-08-19 18:09:22 +0200

    Created interfaces of request parameters and response data fields

---
+++ apiConnection/RequestParametersInterface.ts
@@ -0,0 +1,25 @@
+interface RequestParametersIterface {
+    'date-min'?: string
+    'date-max'?: string
+    'dist-min'?: string
+    'dist-max'?: string
+    'h-min'?: number
+    'h-max'?: number
+    'v-inf-min'?: number
+    'v-inf-max'?: number
+    'v-rel-min'?: number
+    'v-rel-max'?: number
+    'class'?: string
+    'pha'?: boolean
+    'nea'?: boolean
+    'comet'?: boolean
+    'nea-comet'?: boolean
+    'neo'?: boolean
+    'kind'?: string
+    'spk'?: number
+    'des'?: string
+    'body'?: string
+    'sort'?: string
+    'limit'?: number
+    'fullname'?: boolean
+}
---
+++ apiConnection/ResponseDataFields.ts
@@ -0,0 +1,15 @@
+export interface ResponseDataFields {
+    'des': string
+    'orbit_id': number
+    'jd':string
+    'cd':string
+    'dist':number
+    'dist_min':number
+    'dist_max':number
+    'v_rel':number
+    'v_inf':number
+    't_sigma_f':string
+    'body':string
+    'h':number
+    'fullname': string
+}
