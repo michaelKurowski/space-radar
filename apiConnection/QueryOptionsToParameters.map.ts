6a71f614f28b1dd334567d0459a45a42963150dd
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2018-09-02 20:21:12 +0200

    fixed confused cache

--- apiConnection/QueryOptionsToParameters.map.ts
+++ apiConnection/QueryOptionsToParameters.map.ts
@@ -5,7 +5,6 @@ const nameof = <T>(name: keyof T) => name
 
 export default new Map<keyof QueryOptions, keyof RequestParameters>([
     [nameof<QueryOptions>('dateFrom'), nameof<RequestParameters>('date-min') ],
-    [nameof<QueryOptions>('dateTo'), nameof<RequestParameters>('date-max')],
-    [nameof<QueryOptions>('distanceMin'), nameof<RequestParameters>('dist-min')],
-    [nameof<QueryOptions>('body'), nameof<RequestParameters>('body')]
+    [nameof<QueryOptions>('dateTo'), nameof<RequestParameters>('date-max')]
+    
 ])
