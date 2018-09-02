2286ffa5ec7c35265e7a4f6976b3c71992717477
michaelKurowski <Byamarro@gmail.com>
2018-09-02 21:31:44 +0200

    Added visibility property to Threat class

--- core/threatsService/threat.class.ts
+++ core/threatsService/threat.class.ts
@@ -1,11 +1,12 @@
 import DangerLevel from './dangerLevels.enum'
+import Visibility from './visibility.enum';
 
 class Threat {
     private dateMin: Date
     private dateMax: Date
     private dangerLevel: DangerLevel
 
-    constructor( dateMin: Date, dateMax: Date, dangerLevel: DangerLevel) {
+    constructor( dateMin: Date, dateMax: Date, dangerLevel: DangerLevel, private visibility: Visibility) {
         this.dateMin = dateMin
         this.dateMax = dateMax
         this.dangerLevel = dangerLevel
