02b848be1451ce324d4f9419a06d3df4732e1841
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2018-09-08 19:34:53 +0200

    Recieving asynchronus response from nasa api

--- apiConnection/responseParameters.interface.ts
+++ apiConnection/responseParameters.interface.ts
@@ -1,15 +1,15 @@
 export default interface ResponseDataFields {
-    'des'?: string
-    'orbit_id'?: number
-    'jd'?:string
-    'cd'?:string
-    'dist'?:number
-    'dist_min'?:number
-    'dist_max'?:number
-    'v_rel'?:number
-    'v_inf'?:number
-    't_sigma_f'?:string
-    'body'?:string
-    'h'?:number
-    'fullname'?: string
+    'des'?: String
+    'orbit_id'?: String
+    'jd'?:String
+    'cd'?:String
+    'dist'?:String
+    'dist_min'?:String
+    'dist_max'?:String
+    'v_rel'?:String
+    'v_inf'?:String
+    't_sigma_f'?:String
+    'body'?:String
+    'h'?:String
+    'fullname'?: String
 }
--- core/threatsService/threatsService.class.ts
+++ core/threatsService/threatsService.class.ts
@@ -3,6 +3,8 @@ import Threat from './threat.class'
 import {Observable, Observer} from 'rxjs'
 import ApiConnector from '../../apiConnection/apiConnector.class'
 import moment from 'moment'
+import DangerLevels from './dangerLevels.enum'
+import ResponseParameters from '../../apiConnection/responseParameters.interface'
 class ThreatsService {
     private DATE_FORMAT:string = 'YYYY-MM-DD'
     private apiConnector: ApiConnector
@@ -20,20 +22,22 @@ class ThreatsService {
         this.apiConnector = new ApiConnector()
     }
 
+    private async prepareResponse(): Promise<Threat[]> {
+        return await this.threatsMocks
+    }
 
-    getThreats(days:number): Observable<Threat[]> {
+    getThreats(days:number,body:String, dangerLevel:DangerLevels): Observable<Threat[]> {
         const dateFrom = moment().format(this.DATE_FORMAT) 
         const dateTo = moment().add(days,'days').format(this.DATE_FORMAT)
         const queryOptions: QueryOptions = {
             dateFrom,
             dateTo
         }
+        
 
-        const receivedThreats = this.apiConnector.getThreats(queryOptions)
-
-        const listOfThreats:Threat[] = []
-        return Observable.create((observer: Observer<Threat[]>) => {
-            observer.next(this.threatsMocks)
+        return Observable.create(async (observer: Observer<Threat[]>) => {
+           const response = await this.prepareResponse()
+           observer.next(response)
         } )
     }
 }
